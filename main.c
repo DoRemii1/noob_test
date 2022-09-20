c959a29d73ea1fb418562bd5696149117d6b2f59
DoRemii1 <2105983471@qq.com>
2022-09-20 20:34:49 +0800

    update main.c

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+
+void function(int sum)
+{
+    for(int i=0;i<11;i++){
+        sum += i;
+    }
+    return sum;
+}
 int main()
 {
     printf("hello world");
+    int sum=0;
+    sum = function(sum);
     return 0;
 }
